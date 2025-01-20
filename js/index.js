b5c23bf4ace4ba851b3105f54de71a2502e83592
Alicia Bures <arosebures@gmail.com>
2025-01-19 20:42:58 -0500

    Completed part one and updated readme

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*Write any questions that may arise in this experiment and we can discuss them in class.
+Log your arrays to the console.
+Create an array that holds different data types and log it the console. What happens?
+Log the typeof to one array to the console. What is returned?
+Remove an item from your arrays.
+Add an item from your arrays.
+Change the value of an element in each array. */
+
+let animals = ["tiger", "bear", "eagle",];
+console.log(animals);
+let mixedTypes = ["balloon", 19, "18", true];
+console.log(mixedTypes);
+animals.splice(1, 1);
+console.log(animals);
+animals.push("monkey");
+console.log(animals)
